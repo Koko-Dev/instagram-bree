77ffc031ae5be4919d556cef5093b4861c8aedbb
Koko Dev <koko.webdev@gmail.com>
2018-03-15 16:31:57 -0400

    Add a fetch event listener to sw.js to hijack all fetch requests using respondWith()

--- public/sw.js
+++ public/sw.js
@@ -13,3 +13,22 @@ self.addEventListener('activate', function (event) {
     return self.clients.claim();
 });
 
+// Non-Life-Cycle Event
+
+/* When a fetch event is triggered,  such as an img tag requesting an image,
+*  we hijack the output and respond with whatever we want to respond with.
+*
+*  The point is, the response has to go through the Service Worker
+*
+*  Using fetch(), we can look at a SW as a network proxy.
+*
+*  Use respondsWith() so that every outgoing fetch request and response goes through the SW.
+*/
+self.addEventListener('fetch', function (event) {
+    // fetch event will happen, for example, if html asks for img
+    console.log('[Service Worker] Fetching something ...', event);
+
+    // Use respondsWith() to Override the data which gets sent back
+    event.respondWith(fetch(event.request));
+});
+
