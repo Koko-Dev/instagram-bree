c8be8d4d5d993d5e8c1b5342a1314cbb202cf109
Koko Dev <koko.webdev@gmail.com>
2018-03-15 15:50:48 -0400

    On sw.js, add 'install' and 'activate' event listeners to ensure service worker is running
    To add additional robustness, returned self.clients.claim() to ensure the SW was loaded and activated correctly.

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,15 @@
 // This is a service worker
+// Service workers react to specific events, but no DOM access
+
+self.addEventListener('install', function (event) {
+    console.log('[Service Worker] Installing Servent Worker ...', event);
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('[Service Worker] Activating Servent Worker ...', event);
+
+    // self.clients.clain() ensures that SWs are loaded or are activated correctly.
+    // Not necessary, but does make the code more robust.
+    return self.clients.claim();
+});
 
