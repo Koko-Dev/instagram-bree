efbfc28695fb3b6ee16e9dfec4123f948afee737
Koko Dev <koko.webdev@gmail.com>
2018-03-16 12:36:20 -0400

    Default Chrome 'add home screen' banner preempted to display after user clicks button to add a post

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -5,4 +7,22 @@ if ('serviceWorker' in navigator) {
             console.log('Service worker is running in app.js!');
         })
 }
+
+// Chrome displays prompt after user visits site again after 5 minutes
+// To prevent this from happening and set our own time we prevent Chrome default
+window.addEventListener('beforeinstallprompt', function (event) {
+    // To prevent Chrome from showing the banner
+
+    console.log('beforeinstallprompt is fired', event);
+
+    // event returns BeforeInstallPromptEvent method prompt() to display banner
+    // will call deferredPrompt.prompt() in feed.js when var shareImageButton is clicked
+    deferredPrompt = event;
+    event.preventDefault();
+
+    // return false to not do anything upon this event
+    return false;
+    
+
+});
 
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,31 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+
+  // Check to see if var deferredPrompt from app.js is set
+    // This will only work IFF Chrome tried to set the prompt first according to their criteria
+    //    which is when user visits app the second time after five minutes
+  if (deferredPrompt) {
+      // show banner to add icon to home screen
+      // remember that deferredPrompt = event, and even returns BeforeInstallPromptEvent
+      //    which has the property prompt()
+      deferredPrompt.prompt();
+
+      //See what the user's choice is (to add to home screen or not)
+      // userChoice is a Promise
+      deferredPrompt.userChoice.then(function (choiceResult) {
+          console.log(choiceResult.outcome);
+
+          if (choiceResult.outcome === 'dismissed') {
+              console.log('User cancelled installation');
+          } else {
+              console.log('User added to home screen');
+          }
+      });
+
+      // because we cannot use deferredPrompt again
+      deferredPrompt = null;
+  }
 }
 
 function closeCreatePostModal() {
